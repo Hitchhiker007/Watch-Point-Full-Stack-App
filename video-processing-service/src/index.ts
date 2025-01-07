940d24a5a05a100007cf0065afb2bfcff5600cff
Will <billywells99@hotmail.com>
2025-01-07 18:23:36 +1100

    error handling for requests

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -8,6 +8,16 @@ app.post("/process-video", (req, res) => {
     //Get path of the input video file from request body
     const inputFilePath = req.body.inputFilePath;
     const outputFilePath = req.body.outputFilePath;
+
+    // error handling
+    // error message 400 means its client sided, gave the wrong params / request
+    if (!inputFilePath) {
+        res.status(400).send("Bad Request: Missing input file path.")
+    }
+    if (!outputFilePath) {
+        res.status(400).send("Bad Request: Missing output file path.")
+    }
+
 });
 
 app.listen(port, () => {
