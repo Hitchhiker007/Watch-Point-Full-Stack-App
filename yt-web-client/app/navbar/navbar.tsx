cf712994fa960cf524acab64ed9f512a044395a3
Will <billywells99@hotmail.com>
2025-01-14 00:35:15 +1100

    fixed issues

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,13 +1,17 @@
 import Image from "@/node_modules/next/image";
 import Link from 'next/link';
-import styles from "./navbar.module.css";
+import styles from "./navbar-module.css";
+
 
 export default function Navbar() {
     return (
         <nav className={styles.nav}>
             <Link href="/">
-                <Image className={styles.logo}
-                    src="/youtube-logo.svg" alt="youtube-logo"></Image>
+                <span className={styles.logoContainer}>
+                    <Image width={90} height={20}
+                        className={styles.logo}
+                        src="/youtube-logo.svg" alt="youtube-logo"></Image>
+                </span>
             </Link>
         </nav>
     );
