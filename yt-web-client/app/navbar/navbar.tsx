dcc6bbe181e7a3f4775c9ada2a1e46b9b7098176
Will <billywells99@hotmail.com>
2025-01-14 00:21:04 +1100

    fixing modules

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,9 +1,14 @@
 import Image from "@/node_modules/next/image";
+import Link from 'next/link';
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
     return (
-        <nav>
-            <Image src="/youtube-logo.svg" alt="youtube-logo"></Image>
+        <nav className={styles.nav}>
+            <Link href="/">
+                <Image className={styles.logo}
+                    src="/youtube-logo.svg" alt="youtube-logo"></Image>
+            </Link>
         </nav>
     );
 }
