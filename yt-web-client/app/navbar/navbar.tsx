f1849dc2ffdd4cb318806c9d8067ac3437ab7b22
Will <billywells99@hotmail.com>
2025-01-14 22:09:38 +1100

    created sign in and sign out states functionality to navbar

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,10 +1,30 @@
+"use client";
+
 import Image from "@/node_modules/next/image";
 import Link from 'next/link';
 import styles from "./navbar-module.css";
 import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
 
 
 export default function Navbar() {
+    // Init user state
+    // managing state in function
+    // javascript closure to research
+    // state within a funciton is still mantained after a function has been executed
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        // cleanup subscription on unmount
+        return () => unsubscribe();
+    });
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
@@ -12,7 +32,10 @@ export default function Navbar() {
                     className={styles.logo}
                     src="/youtube-logo.svg" alt="youtube-logo"></Image>
             </Link>
-            <SignIn />
+
+            {/* // TODO: Add a upload button */}
+
+            <SignIn user={user} />
         </nav>
     );
 }
--- yt-web-client/app/navbar/sign-in.tsx
+++ yt-web-client/app/navbar/sign-in.tsx
@@ -5,16 +5,29 @@ import { Fragment } from "react";
 
 import { signInWithGoogle, signOut } from "../firebase/firebase";
 import styles from './sign-in.module.css';
+import { User } from "firebase/auth";
 
-export default function SignIn() {
+// define interface to set user to either user or null
+interface SignInProps {
+    user: User | null;
+}
+
+// take in a single object user
+// destructing assignment
+export default function SignIn({ user }: SignInProps) {
     return (
         <Fragment>
-            <button className={styles.signin} onClick={signOut}>
-                Sign Out
-            </button>
-            <button className={styles.signin} onClick={signInWithGoogle}>
-                Sign In
-            </button>
+            {user ?
+                (
+                    <button className={styles.signin} onClick={signOut}>
+                        Sign Out
+                    </button>
+                ) : (
+                    <button className={styles.signin} onClick={signInWithGoogle}>
+                        Sign In
+                    </button>
+                )
+            }
         </Fragment>
     )
 }
