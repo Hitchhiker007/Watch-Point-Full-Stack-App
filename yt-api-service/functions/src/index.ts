706293fda9220de791ab3f94d5321b034bd1dd44
Will <billywells99@hotmail.com>
2025-01-25 22:18:37 +1100

    changes

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,26 +1,9 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import * as functions from "firebase-functions/v1";
 import { initializeApp } from "firebase-admin/app";
 import { Firestore } from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 import { Storage } from "@google-cloud/storage";
 import { onCall } from "firebase-functions/v2/https";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//     logger.info("Hello logs!", { structuredData: true });
-//     response.send("Hello from Firebase!");
-// });
 
 initializeApp();
 
@@ -42,28 +25,29 @@ export const createUser = functions.auth.user().onCreate((user) => {
     return;
 });
 
-export const generateUploadUrl = onCall({ maxInstances: 1 }, async (request) => {
-    // Check if the user is authentication
-    if (!request.auth) {
-        throw new functions.https.HttpsError(
-            "failed-precondition",
-            "The function must be called while authenticated."
-        );
-    }
+export const generateUploadUrl =
+    onCall({ maxInstances: 1 }, async (request) => {
+        // Check if the user is authentication
+        if (!request.auth) {
+            throw new functions.https.HttpsError(
+                "failed-precondition",
+                "The function must be called while authenticated."
+            );
+        }
 
-    const auth = request.auth;
-    const data = request.data;
-    const bucket = storage.bucket(rawVideoBucketName);
+        const auth = request.auth;
+        const data = request.data;
+        const bucket = storage.bucket(rawVideoBucketName);
 
-    // Generate a unique filename for upload
-    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+        // Generate a unique filename for upload
+        const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
 
-    // Get a v4 signed URL for uploading file
-    const [url] = await bucket.file(fileName).getSignedUrl({
-        version: "v4",
-        action: "write",
-        expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+        // Get a v4 signed URL for uploading file
+        const [url] = await bucket.file(fileName).getSignedUrl({
+            version: "v4",
+            action: "write",
+            expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+        });
+
+        return { url, fileName };
     });
-
-    return { url, fileName };
-});
