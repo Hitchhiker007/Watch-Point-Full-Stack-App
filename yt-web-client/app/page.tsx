3732987e70bad622eaf394f110404715e3bdf3b9
Will <billywells99@hotmail.com>
2025-01-29 17:35:44 +1100

    cleanup

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -9,17 +9,15 @@ export default async function Home() {
   const videos = await getVideos();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {
-          videos.map((video) => (
-            <Link href={`/watch?v=${video.filename}`}>
-              <Image src={'/thumbnail.png'} alt='video' width={120} height={80}
-                className={styles.thumbnail} />
-            </Link>
-          ))
-        }
-      </main>
-    </div>
+    <main>
+      {
+        videos.map((video) => (
+          <Link href={`/watch?v=${video.filename}`}>
+            <Image src={'/thumbnail.png'} alt='video' width={120} height={80}
+              className={styles.thumbnail} />
+          </Link>
+        ))
+      }
+    </main>
   );
 }
