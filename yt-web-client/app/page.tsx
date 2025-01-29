9462839ba7538c79c70f56c6f522093452d95982
Will <billywells99@hotmail.com>
2025-01-29 19:23:24 +1100

    comment

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link';
 
 export default async function Home() {
 
+  // TODO:
+  // add filter to filter out videos still processing
   const videos = await getVideos();
 
   return (
