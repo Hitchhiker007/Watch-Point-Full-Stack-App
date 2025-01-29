3b548fe52c624a521cf7a68f85634d4312fe2e46
Will <billywells99@hotmail.com>
2025-01-29 17:25:46 +1100

    getVideos() test

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -1,6 +1,13 @@
+import { getVideos } from "./firebase/functions";
+import styles from './page.module.css'
 
 
-export default function Home() {
+export default async function Home() {
+
+  const videos = await getVideos();
+
+  console.log(videos);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
