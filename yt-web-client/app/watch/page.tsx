1a93cc004b4de776a8bd8a97e9b740181bb27b03
Will <billywells99@hotmail.com>
2025-01-29 17:46:52 +1100

    videos watch page complete gets videos from bucket

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 import styles from "./watch.module.css";
+import { useSearchParams } from "next/navigation";
 
 export default function Watch() {
+
+    const videoPrefix = 'https://storage.googleapis.com/major-will-processed-videos/';
+    const videoSource = useSearchParams().get('v');
+
     return (
         <div className={styles.container}>
             <h1>Watch Page</h1>
             <div className={styles.videoPlayer}>
                 <video controls width="640" height="360">
-                    <source src="/sample-video.mp4" type="video/mp4" />
+                    <source src={videoPrefix + videoSource} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
